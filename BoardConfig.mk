9caa0aad069697c494c6760b1f1c3719d286bc24
Ashwin <ashwinr64@gmail.com>
2015-09-18 07:06:06 +0530

    Update kernel info and FORCE_32_BIT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,8 @@
 
 DEVICE_PATH := device/alcatel/idol347
 
+FORCE_32_BIT := true
+
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Platform
@@ -55,12 +57,12 @@ TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_BASE := 80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.bootloader=L1AB2020BQ00 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 00000100
+BOARD_RAMDISK_OFFSET     := 01000000
 ifneq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
