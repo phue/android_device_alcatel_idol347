6f2dbb2292a42c8daf9a6abb2d1a0c1ee42f1c4b
petrov0 <ico.petrov.0@gmail.com>
2015-09-23 20:25:40 +0000

    Clean the default properties.

--- device.mk
+++ device.mk
@@ -85,14 +85,9 @@ PRODUCT_PACKAGES += \
 
 # Charger
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.usb.id.charge=3009 \
-    ro.usb.id.mtp=3003 \
-    ro.usb.id.mtp_adb=3004 \
-    ro.usb.id.ptp=3005 \
-    ro.usb.id.ptp_adb=3006 \
-    ro.usb.id.ums=3007 \
-    ro.usb.id.ums_adb=3008 \
-    ro.usb.vid=2a96
+				 persist.sys.usb.config=mtp,adb \
+				 ro.adb.secure=0 \
+				 ro.debuggable=1 
 
 PRODUCT_PACKAGES += \
     charger_res_images
