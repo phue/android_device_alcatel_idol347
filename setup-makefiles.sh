7ae48693d15e4154827d48f4ba3a22e331f7e428
pivoq <patrick.huether@gmail.com>
2015-11-09 17:38:48 +0100

    add qualcomm location
    Change-Id: Ibddeb56709dd22cdfd4ab2aab08dc4a7b736d036

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -159,16 +159,16 @@ LOCAL_PATH := \$(call my-dir)
 ifeq (\$(TARGET_DEVICE),idol347)
 ifeq (\$(QCPATH),)
 
-####include \$(CLEAR_VARS)
-#LOCAL_MODULE := com.qualcomm.location
-#LOCAL_MODULE_OWNER := $VENDOR
-#LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_PRIVILEGED_MODULE := true
-#LOCAL_CERTIFICATE := platform
-#include \$(BUILD_PREBUILT)
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
 
 #include \$(CLEAR_VARS)
 #LOCAL_MODULE := qcrilmsgtunnel
